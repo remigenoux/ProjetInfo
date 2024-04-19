3d1d8d40750fe26fb49716b6f3d9c4957031c612
remigenoux <166151010+remigenoux@users.noreply.github.com>
2024-04-20 00:01:34 +0200

    Update Billard.py

--- projet info/Billard.py
+++ projet info/Billard.py
@@ -1,15 +1,51 @@
 import numpy as np
 import time
+from numpy.random import randint
+
 
 from Billes import *
+def dist(u,v):
+    return np.sqrt((u[0]-v[0])**2+(u[1]-v[1])**2)
 
-def dist(bille1,bille2):
-    bi
 class Billard (list):
     def __init__(self,xmax,ymax,nb_billes):
         self.xmax = xmax
         self.ymax = ymax
         nb_billes += nb_billes%2
-        for i in range (nb_billes/2):
-            self.append(Bleue(randint(0, xmax), randint(0, ymax)))
-            self.append(Rouge(randint(0, xmax), randint(0, ymax)))
+        self.append(Blanche(xmax//2,ymax//2))
+        r= Bille.taille
+        while len(self)<=(nb_billes/2):
+            x1,y1 = randint(0+r, xmax-r), randint(0+r, ymax-r )
+            Plein=False
+            for j in range (len(self)):
+                if dist(self[j],(x1,y1))<r :
+                    Plein = True
+            if not Plein :
+                self.append(Bleue(x1,y1))
+        while len(self) <= (nb_billes):
+            x2,y2 = randint(0+r, xmax-r ), randint(0+r, ymax-r)
+            Plein = False
+            for j in range(len(self)):
+                if dist(self[j], (x2,y2)) < r:
+                    Plein = True
+            if not Plein:
+                self.append(Rouge(x2, y2))
+    def UnTour(self,J):
+        return 0
+
+    def simuler(self):
+        tour=1
+        J = 'Bleu'
+        while not self.fin_partie():
+            print("c'est le tour",tour)
+            self.UnTour(J)
+            if J == 'Bleu':
+                J='Rouge'
+            else :
+                J = 'Bleu'
+            tour+=1
+
+
+
+
+
