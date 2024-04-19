4c03cee496973fc3b7ce3042333a5acd6f798bc0
remigenoux <166151010+remigenoux@users.noreply.github.com>
2024-04-20 00:06:26 +0200

    Update Billes.py
    20/04 minuit parce qu'on est en retarddddd

--- projet info/Billes.py
+++ projet info/Billes.py
@@ -1,9 +1,9 @@
 import numpy as  np
-from matplotlib.pyplot as plt
 
+from Billard import *
 def scalaire(u,v):
     return u[0]*v[0]+u[1]*v[1]
-def norme(u)
+def norme(u):
     return np.sqrt(u[0]**2+u[1]**2)
 
 class Bille () :
@@ -17,7 +17,11 @@ class Bille () :
 
     def __str__(self) :
         return (self.coul,self.coords,self.v)
+    def x(self):
+        return self.direction[0]
 
+    def y(self):
+        return self.direction[1]
     def coul(self):
         return 'B'
 
@@ -28,14 +32,14 @@ class Bille () :
         return self.v
 
     def axe(self, boule2):
-        axe = (self.x-boule2.x,self.y-boule2-y)
+        axe = (self.x-boule2.x,self.y-boule2.y)
         axe = axe/norme(axe)
-        if scalaire(axe,direction) < 0 :
+        if scalaire(axe,self.direction) < 0 :
             axe = -axe
         return axe
 
     def contact_boule(self, boule2):
-        axe = axe(self,boule2)
+        axe = self.axe(boule2)
         if self.attenuation() == 0 :
             boule2.x += axe[0]
             boule2.y += axe[1]
@@ -44,9 +48,35 @@ class Bille () :
             boule2.v = scalaire(axe,self.direction)*self.v
             boule2.direction = axe
             self.direction = (self.direction-axe)/norme(self.direction-axe)
+
+    def contact_mur(self):
+        self.v = self.attenuation()
+        x,y= self.x,self.y
+        r = self.taille
+        cote = []
+        if x <= r :
+            cote.append('g')
+        if x >= Billard.xmax-r :
+            cote.append('d')
+        if y<=r :
+            cote.append('b')
+        if y>= Billard.ymax-r:
+            cote.append('h')
+        for e in cote :
+            if e == 'g' or e =='d':
+                self.x = - self.x
+            if e == 'h' or e == 'b':
+                self.y=-self.y
+
+
+
+
+"""
 class Blanche (Bille):
-    def coul(self):
-        return 'Blanche'
+    def __init__(self,abscisse,ordonnee,vitesse=0,direction=(0,0),taille=5,attribut): 
+        super().__init__(abscisse,ordonnee,vitesse=0,direction=(0,0),taille=5)
+        self.attribut = attribut"""
+
 class Bleue (Bille):
     def coul(self):
         return 'Bleue'
