316af8f3cee23fd8ab501e35cc01c25f9b610078
remigenoux <166151010+remigenoux@users.noreply.github.com>
2024-04-22 17:08:21 +0200

    Update Billard.py
    Changement fonction UnTour + pas de temps dans Billard

--- projet info/Billard.py
+++ projet info/Billard.py
@@ -8,9 +8,10 @@ def dist(u,v):
     return np.sqrt((u[0]-v[0])**2+(u[1]-v[1])**2)
 
 class Billard (list):
-    def __init__(self,xmax,ymax,nb_billes):
+    def __init__(self,xmax,ymax,nb_billes,dt):
         self.xmax = xmax
         self.ymax = ymax
+        self.dt = dt
         nb_billes += nb_billes%2
         self.append(Blanche(xmax//2,ymax//2))
         r= Bille.taille
@@ -31,27 +32,29 @@ class Billard (list):
             if not Plein:
                 self.append(Rouge(x2, y2))
     def UnTour(self,J):
-        """force = input("veuillez entrer une vitesse")
+        force = input("veuillez entrer une vitesse")
         x = input("veuillez entrer une direction x")
         y = input ("veuillez entrer une direction y")
-        try
+        try :
             force = int(force)
             direction = (int(x),int(y))
         except ValueError:
             print("Vous n'avez pas répondu correctement aux questions")
-        Blanche.attribut = J"""
 
-
-        #   on attribue la sous couleur "bleue ou rouge"
+        if J == 'IA' :
+            Blanche.attribut = 'Rouge' #   on attribue la sous couleur "bleue ou rouge"
+        else :
+            Blanche.attribut = J
+        mouvement = []                      #IL FAUT METTRE LA BLANCHE DEDANS
+        while len(mouvement)!= 0 :
+            simulation()
         #   on met la boule blanche dans la liste mouvement
         #   tant que au moins une bille est en mouvement : simulation()
-        #   on récupère les nouvelles positions
-        return 0
+        #   on change les positions dans Billard
 
     def simulation(self):
-        # On définit un pas de temps
         # on étudie les positions de toutes les billes en mouvement (liste) après ce pas de temps
-        # on étbalit les potentiels contacts
+        # on établit les potentiels contacts
         # on établit le temps des contacts
         # on gère le contacts avec les fonctions qu'on connaît déjà
         # on avance toute la simulation du temps
@@ -60,10 +63,9 @@ class Billard (list):
 
 
     def partie(self):
-        nbr = input("Voulez-vous jouer contre l'ordinateur ? (1=oui,2=non)")
+        nbr = input("Voulez-vous jouer contre un ami ? (1=oui,2=non)")
         try:
             Joueur = int(nbr)
-            print("La valeur entree est un entier = ", val)
         except ValueError:
             print("Vous n'avez pas répondu correctement à la question")
         tour=1
@@ -71,12 +73,12 @@ class Billard (list):
         while not self.fin_partie():
             print("c'est le tour",tour,"c'est à ",J,"de jouer")
             self.UnTour(J)
-            if val == 1 :
+            if Joueur == 1 :
                 if J == 'Bleu':
                     J='Rouge'
                 else :
                     J = 'Bleu'
-            elif val == 2 :
+            elif Joueur == 2 :
                 if J == 'Bleu':
                     J='IA'
                 else :
